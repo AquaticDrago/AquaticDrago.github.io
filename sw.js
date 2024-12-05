a0c96efdbd9f01745b7b929acb6af2212f000ed6
AquaticDrago <125418341+AquaticDrago@users.noreply.github.com>
2024-12-05 01:30:25 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('push', event => {
     const options = {
         body: event.data.text(),
         icon: 'perro.png',  // Ruta al icono en la raíz
-        badge: 'images/notification_badge.png', // Si tienes un badge, especifica la ruta aquí
+        badge: 'download.jpeg',
     };
 
     event.waitUntil(
