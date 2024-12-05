6285b598188fec156f485e83eb5d8514ed1f6fbf
AquaticDrago <125418341+AquaticDrago@users.noreply.github.com>
2024-12-05 00:07:36 -0600

    Update push.js

--- push.js
+++ push.js
@@ -1,10 +1,9 @@
 // Solicitar permisos para las notificaciones
 if ('Notification' in window && 'serviceWorker' in navigator) {
     Notification.requestPermission().then(permission => {
-        if (permission === 'granted') {
-            console.log('Permisos para notificaciones concedidos');
-        } else {
-            console.log('Permisos para notificaciones denegados');
+        console.log('Permisos de notificación:', permission);
+        if (permission !== 'granted') {
+            alert('Debes conceder permisos para recibir notificaciones.');
         }
     });
 }
