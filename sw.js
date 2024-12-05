f20cd97b48f3f9eabc68910c973e8426ea7e6835
AquaticDrago <125418341+AquaticDrago@users.noreply.github.com>
2024-12-05 00:40:54 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,16 @@
 self.addEventListener('install', event => {
-    console.log('Service Worker instalado correctamente.');
-    event.waitUntil(
-        caches.open('static-v1').then(cache => {
-            return cache.addAll(['./', './index.php', './manifest.json']);
-        })
-    );
+    console.log('Service Worker instalado.');
+    event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('fetch', event => {
-    console.log('Interceptando petición:', event.request.url);
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+self.addEventListener('activate', event => {
+    console.log('Service Worker activado.');
+    event.waitUntil(self.clients.claim());
 });
 
-// Manejo de las notificaciones y acciones
 self.addEventListener('notificationclick', event => {
-    const action = event.action;
-
-    if (action === 'comprar') {
-        clients.openWindow('https://www.tienda-de-mascotas.com'); // Cambia por tu URL
-    } else if (action === 'cerrar') {
-        event.notification.close();
-    } else {
-        event.notification.close(); // Acción por defecto
-    }
-
+    event.notification.close();
     event.waitUntil(
-        clients.matchAll({ type: 'window' }).then(windowClients => {
-            if (windowClients.length > 0) {
-                windowClients[0].focus();
-            }
-        })
+        clients.openWindow('https://vercel.com/') // URL a abrir al hacer clic
     );
 });
